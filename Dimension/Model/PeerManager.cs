6f14363eb38bf2ac90d42e69258d7531b0349891
Lewis <lewis@mixav.com.au>
2017-12-28 22:32:44 +1100

    Peer update events

--- Dimension/Model/PeerManager.cs
+++ Dimension/Model/PeerManager.cs
@@ -8,27 +8,43 @@ namespace Dimension.Model
 {
     class PeerManager
     {
+        public Peer[] allPeers
+        {
+            get
+            {
+                lock (peers)
+                    return ((new List<Peer>(peers.Values)).ToArray());
+            }
+        }
+        public delegate void PeerUpdateEvent(Peer p);
+        public event PeerUpdateEvent peerRenamed;
+        public event PeerUpdateEvent peerAdded;
         Dictionary<ulong, Peer> peers = new Dictionary<ulong, Peer>();
         public void parseHello(Commands.HelloCommand h, System.Net.IPEndPoint sender)
         {
-            if (peers.ContainsKey(h.id))
+            lock (peers)
             {
-                peers[h.id].actualEndpoint = sender;
-                peers[h.id].publicAddress = System.Net.IPAddress.Parse(h.externalIP);
-                if (peers[h.id].username != h.username)
+                if (peers.ContainsKey(h.id))
                 {
-                    peers[h.id].username = h.username;
-                    //TODO: Trigger event for username change
+                    peers[h.id].actualEndpoint = sender;
+                    peers[h.id].publicAddress = System.Net.IPAddress.Parse(h.externalIP);
+                    if (peers[h.id].username != h.username)
+                    {
+                        peers[h.id].username = h.username;
+                        if (peerRenamed != null)
+                            peerRenamed(peers[h.id]);
+                    }
                 }
-            }
-            else
-            {
-                peers[h.id] = new Peer();
-                peers[h.id].id = h.id;
-                peers[h.id].actualEndpoint = sender;
-                peers[h.id].publicAddress = System.Net.IPAddress.Parse(h.externalIP);
-                peers[h.id].username = h.username;
-                //TODO: Trigger event for new peer found
+                else
+                {
+                    peers[h.id] = new Peer();
+                    peers[h.id].id = h.id;
+                    peers[h.id].actualEndpoint = sender;
+                    peers[h.id].publicAddress = System.Net.IPAddress.Parse(h.externalIP);
+                    peers[h.id].username = h.username;
+                    if (peerAdded != null)
+                        peerAdded(peers[h.id]);
+                }
             }
         }
     }
