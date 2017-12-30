cd82f390590fd4d8b45ecb89e9a2aa1e3c7e9fe5
Lewis <lewis@mixav.com.au>
2017-12-30 17:45:05 +1100

    Hopefully that covers UDT sockets

--- Dimension/Model/UdtOutgoingConnection.cs
+++ Dimension/Model/UdtOutgoingConnection.cs
@@ -11,19 +11,41 @@ namespace Dimension.Model
         public override event CommandReceived commandReceived;
         public UdtOutgoingConnection(System.Net.IPAddress addr, int port)
         {
-
+            socket = new Udt.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream);
+            socket.Connect(new System.Net.IPEndPoint(addr, port));
+            System.Threading.Thread t = new System.Threading.Thread(receiveLoop);
+            t.IsBackground = true;
+            t.Name = "UDT receive loop";
+            t.Start();
         }
+        void receiveLoop()
+        {
+            while (isConnected)
+            {
+                byte[] lenByte = new byte[4];
+                socket.Receive(lenByte);
+                byte[] dataByte = new byte[BitConverter.ToInt32(lenByte, 0)];
+                socket.Receive(dataByte);
 
-
+                commandReceived?.Invoke(Program.serializer.deserialize(dataByte));
+            }
+        }
+        bool isConnected = true;
+        Udt.Socket socket;
         public override void send(Commands.Command c)
         {
-
+            byte[] b = Program.serializer.serialize(c);
+            int len = b.Length;
+            socket.Send(BitConverter.GetBytes(len), 0, 4);
+            socket.Send(b, 0, b.Length);
         }
         public override bool connected
         {
             get
             {
-                return false;
+                if (socket.State == Udt.SocketState.Closed || socket.State == Udt.SocketState.Closing)
+                    isConnected = false;
+                return isConnected;
             }
         }
     }
