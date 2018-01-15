c863054b80e35722f7229ff82a1d7bce414a8fe4
Lewis Hosie <lewis@mixav.com.au>
2018-01-15 19:35:14 +1100

    Click a link to join a bootstrap! Neato.

--- Dimension/UI/HTMLPanel.cs
+++ Dimension/UI/HTMLPanel.cs
@@ -19,9 +19,20 @@ namespace Dimension.UI
 
             WebBrowser p = new WebBrowser();
             p.Dock = DockStyle.Fill;
+            p.Navigating += navigate;
             Controls.Add(p);
-            p.DocumentText = "<h1>Testing!</h1><p>Oh dear, what hath God wrought...</p>";
+            p.DocumentText = "<html><body><h1>Testing!</h1><p>Oh dear, what hath God wrought...</p> <h2>List of Bootsraps</h2><ul><li><a href='!BOOTSTRAP!http://www.9thcircle.net/Projects/Dimension/bootstrap.php'>9th Circle Test Bootstrap</a></li><li><a href='!BOOTSTRAP!http://www.respawn.com.au/dimension.php'>Respawn LAN Bootstrap</a></li></ul></body></html>";
             p.Refresh(WebBrowserRefreshOption.Completely);
         }
-    }
+        void navigate(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (e.Url.AbsolutePath.StartsWith("!BOOTSTRAP!"))
+            {
+                e.Cancel = true;
+                string s = e.Url.AbsolutePath;
+                s = s.Substring("!BOOTSTRAP!".Length);
+                UI.JoinCircleForm.joinCircle(s, JoinCircleForm.CircleType.bootstrap);
+            }
+            }
+        }
 }
