95c23537926274ac088b4bf8cb530e4e6048a8df
Lewis Hosie <lewis@mixav.com.au>
2018-01-21 15:41:47 +1100

    Improved admin elevation system for updater (only requires admin when actually needed, and doesn't run Dimension as admin). Also added new icon.

--- Updater/UpdatingForm.cs
+++ Updater/UpdatingForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Principal;
 
 namespace Updater
 {
@@ -17,31 +18,73 @@ namespace Updater
             InitializeComponent();
             System.Threading.Thread t = new System.Threading.Thread(delegate ()
             {
-                System.Net.WebClient w = new System.Net.WebClient();
-                byte[] b = w.DownloadData("http://9cstatic.nfshost.com/Dimension/" + url);
+                try
+                {
+                    System.Net.WebClient w = new System.Net.WebClient();
+                    byte[] b = w.DownloadData("http://9cstatic.nfshost.com/Dimension/" + url);
+                    string tempFolder = System.IO.Path.GetTempPath();
 
-                if (System.IO.File.Exists("newversion.zip"))
-                    System.IO.File.Delete("newversion.zip");
-                System.IO.File.WriteAllBytes("newversion.zip", b);
-                if (System.IO.Directory.Exists("temp"))
-                    System.IO.Directory.Delete("temp", true);
-                System.IO.Directory.CreateDirectory("temp");
-                System.IO.Compression.ZipFile.ExtractToDirectory("newversion.zip", "temp");
-                foreach (System.IO.FileInfo f in new System.IO.DirectoryInfo("temp").GetFiles())
-                    if (f.Name != "Updater.exe")
-                        System.IO.File.Copy(f.FullName, f.Name, true);
+                    if (System.IO.File.Exists(System.IO.Path.Combine(tempFolder, "newversion.zip")))
+                        System.IO.File.Delete(System.IO.Path.Combine(tempFolder, "newversion.zip"));
+                    System.IO.File.WriteAllBytes(System.IO.Path.Combine(tempFolder, "newversion.zip"), b);
+                    if (System.IO.Directory.Exists(System.IO.Path.Combine(tempFolder, "DimensionTemp")))
+                        System.IO.Directory.Delete(System.IO.Path.Combine(tempFolder, "DimensionTemp"), true);
+                    System.IO.Directory.CreateDirectory(System.IO.Path.Combine(tempFolder, "DimensionTemp"));
+                    System.IO.Compression.ZipFile.ExtractToDirectory(System.IO.Path.Combine(tempFolder, "newversion.zip"), System.IO.Path.Combine(tempFolder, "DimensionTemp"));
+                    foreach (System.IO.FileInfo f in new System.IO.DirectoryInfo(System.IO.Path.Combine(tempFolder, "DimensionTemp")).GetFiles())
+                        if (f.Name != "Updater.exe")
+                            System.IO.File.Copy(f.FullName, f.Name, true);
 
 
-                if (System.IO.Directory.Exists("temp"))
-                    System.IO.Directory.Delete("temp",true);
-                if (System.IO.File.Exists("newversion.zip"))
-                    System.IO.File.Delete("newversion.zip");
-                System.Diagnostics.Process.Start("Dimension.exe");
-                Application.Exit();
+                    if (System.IO.Directory.Exists(System.IO.Path.Combine(tempFolder, "DimensionTemp")))
+                        System.IO.Directory.Delete(System.IO.Path.Combine(tempFolder, "DimensionTemp"), true);
+                    if (System.IO.File.Exists(System.IO.Path.Combine(tempFolder, "newversion.zip")))
+                        System.IO.File.Delete(System.IO.Path.Combine(tempFolder, "newversion.zip"));
+                    if (!isAdmin())
+                        System.Diagnostics.Process.Start("Dimension.exe");
+                    Application.Exit();
+                }
+                catch (AccessViolationException)
+                {
+                    elevate();
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    elevate();
+                }
             });
             t.IsBackground = true;
             t.Name = "Download thread";
             t.Start();
         }
-    }
+        static void elevate()
+        {
+
+            if (isAdmin())
+            {
+                MessageBox.Show("Error - could not access temp folder. Failing...");
+                Application.Exit();
+                return;
+
+            }
+            else
+            {
+                System.Diagnostics.Process p = new System.Diagnostics.Process();
+                p.StartInfo.FileName = System.Reflection.Assembly.GetEntryAssembly().Location;
+                p.StartInfo.Verb = "runas";
+                p.StartInfo.UseShellExecute = true;
+                p.Start();
+
+                p.WaitForExit();
+                System.Diagnostics.Process.Start("Dimension.exe");
+
+            }
+        }
+        static bool isAdmin()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        }
 }
